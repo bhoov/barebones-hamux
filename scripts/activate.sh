baaf3af1286e65a4512abee6119f4d5e4eadecb5
Ben Hoover <benhoover34@gmail.com>
2025-05-23 09:25:33 -0400

    Update activation script to allow calling from docs

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -3,4 +3,6 @@
 
 # Try to deactivate conda, but don't fail if it's not available
 command -v conda >/dev/null 2>&1 && conda deactivate
-source .venv/bin/activate
+
+# .venv lives in the parent's parent directory
+source $(dirname $(dirname "$BASH_SOURCE"))/.venv/bin/activate
